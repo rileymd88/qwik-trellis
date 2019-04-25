6c7fc5e27f7640f3ce7c11285fe4d935b0350d88
Purwa Shrivastava <atq@qliktech.com>
2019-04-25 10:07:42 +0200

    DEB-196/spellingMistake : Correcting the spelling in the Add-ons section.

--- src/definition.js
+++ src/definition.js
@@ -57,7 +57,7 @@ define([], function () {
 
   var link = {
     ref: "prop.vizId",
-    label: "Base Vizualisation",
+    label: "Base Visualization",
     type: "string",
     component: "dropdown",
     options: function () {
